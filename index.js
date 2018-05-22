6e54b31bb0fa83bab8dc04d0e8507b4a312762bb
yhtomitim <33264110+yhtomitim@users.noreply.github.com>
2018-05-22 15:57:13 -0600

    use express, create index route

--- index.js
+++ index.js
@@ -2,4 +2,14 @@ const express = require('express');
 const cors = require('cors');
 const port = process.env.PORT || 5000;
 
+const app = express();
+ 
+app.use(cors());
+
+app.get('/', (req, res) => {
+  res.json({ message: 'this is the index GET route' });
+});
+
+app.listen(port, () => console.log(`Server is listening on port ${port}`));
+
 

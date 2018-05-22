4f17f1d323e8f05b2655723a8621e999b4864472
yhtomitim <33264110+yhtomitim@users.noreply.github.com>
2018-05-22 16:00:39 -0600

    add student data

---
+++ students.js
@@ -0,0 +1,34 @@
+module.exports = {
+  data: [
+    {
+      'id': 1,
+      'firstName': 'Alice',
+      'lastName': 'Zephyr',
+      'homeTown': 'Seattle'
+    },
+    {
+      'id': 2,
+      'firstName': 'Bob',
+      'lastName': 'Yellow',
+      'homeTown': 'Vancouver'
+    },
+    {
+      'id': 3,
+      'firstName': 'Claire',
+      'lastName': 'Xylitol',
+      'homeTown': 'Toledo'
+    },
+    {
+      'id': 4,
+      'firstName': 'Daniel',
+      'lastName': 'Winston',
+      'homeTown': 'Akron'
+    },
+    {
+      'id': 5,
+      'firstName': 'Edina',
+      'lastName': 'Veritas',
+      'homeTown': 'Wichita'
+    }
+  ]
+}

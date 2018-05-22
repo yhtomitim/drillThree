910d930e71fb9445a613779c42c401b68d8b6d0b
yhtomitim <33264110+yhtomitim@users.noreply.github.com>
2018-05-22 16:00:51 -0600

    import student data, add to index GET route

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const cors = require('cors');
 const port = process.env.PORT || 5000;
+const data = require('./students');
 
 const app = express();
  
 app.use(cors());
 
 app.get('/', (req, res) => {
-  res.json({ message: 'this is the index GET route' });
+  res.json(data);
 });
 
 app.listen(port, () => console.log(`Server is listening on port ${port}`));
